e27954ad16110a10ca51143558bd1f447316c3c0
jekeande <jekeande@gmail.com>
2022-07-20 11:22:58 -0300

    commit

--- node-loja/data/CRUD/cliente.js
+++ node-loja/data/CRUD/cliente.js
@@ -1,32 +1,17 @@
-const express = require('express')
-const sqlite = require('sqlite')
-const app = express();
+import ConnectionDb from './ConnectionDb'
 
-app.use(express.json());
-
-const database = new sqlite.Database('./data_loja', (err) => {
-    if (err) {
-        console.log("Erro ao abrir base de dados " + err.message);
-    } else {
-        console.log("Conectado com o Banco de Dados")
-    }
-})
-
-app.post("/clientes", (req, res, next) => {
-    database.run("INSERT INTO CLIENTE (cpf_cliente,nome_cliente,endereco_cliente,cep_cliente,email_cliente,senha_cliente) VALUES(?,?,?,?,?,?)",
-    [req.body.cpf, req.body.nome, req.body.endereco, req.body.cep, req.body.email, req.body.senha],
+export default function Cliente() {
+app.post("/cliente", (req, res, next) => {
+    ConnectionDb.run("INSERT INTO cliente (cpf_cliente,nome_cliente,endereco_cliente,cep_cliente,email_cliente,senha_cliente)VALUES(?,?,?,?,?,?)",
+    [req.body.cpf,req.body.nome,req.body.endereco,req.body.cep,req.body.email,req.body.senha],
     function(err, result){
         if(err) {
             res.status(400).json({ "error": err.message })
-            return;
+            return; 
         }
         res.status(201).json({
             "ID": this.lastID
         })
     })
 })
-
-
-app.listen(3306, () => {
-    console.log('Iniciando o ExpressJS na porta 3000')
-})
+}
--- node-loja/index.js
+++ node-loja/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const sqlite3 = require('sqlite3')
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 const db = new sqlite3.Database('./data/data_loja.db', (err) => {
     if (err) {
@@ -13,8 +16,10 @@ const db = new sqlite3.Database('./data/data_loja.db', (err) => {
 })
 
 app.post("/cliente", (req, res, next) => {
+    const senhaCriptografada = bcrypt.hashSync(req.body.senha, salt);
+    console.log("Senha Mascarada: ", senhaCriptografada)
     db.run("INSERT INTO cliente (cpf_cliente,nome_cliente,endereco_cliente,cep_cliente,email_cliente,senha_cliente)VALUES(?,?,?,?,?,?)",
-    [req.body.cpf,req.body.nome,req.body.endereco,req.body.cep,req.body.email,req.body.senha],
+    [req.body.cpf,req.body.nome,req.body.endereco,req.body.cep,req.body.email, senhaCriptografada],
     function(err, result){
         if(err) {
             res.status(400).json({ "error": err.message })
@@ -25,6 +30,7 @@ app.post("/cliente", (req, res, next) => {
         })
     })
 })
+
 
 
 app.listen(3306, () => {
