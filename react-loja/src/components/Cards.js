dab04b7a35839474dfac8168b76e25fa20040441
jekeande <jekeande@gmail.com>
2022-07-27 21:06:54 -0300

    commit

--- react-loja/src/components/Cards.js
+++ react-loja/src/components/Cards.js
@@ -8,9 +8,10 @@ export default function Cards({produto}) {
             {produto.foto_produto}
         </div>
         <div className="descricao_produto">
-          <p>Nome: {produto.nome_produto}</p>
+          <p>{produto.nome_produto}</p>
+          <hr/>
+          <p>{produto.tipo_produto}</p>
           <p>Tamanho: {produto.tamanho_produto}</p>
-          <p>Tipo: {produto.tipo_produto}</p>
           <p>Preco: {produto.valor_produto}</p>
         </div>
       </div>
